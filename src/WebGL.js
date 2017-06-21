e85c2baec9f1cdcf16d1e44277487d1bc14c1d4c
chentingliang <tingliang1024@gmail.com>
2017-06-21 16:53:29 +0800

    Separating HTML and JavaScript

---
+++ src/WebGL.js
@@ -0,0 +1,21 @@
+'use strict';
+var gGL = null;
+
+function initializeGL() {
+    var canvas = document.getElementById('GLCanvas');
+    gGL = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    if (gGL !== null) {
+        gGL.clearColor(0.0, 0.8, 0.0, 1.0);
+    } else {
+        document.write('<br><b>WebGL is not supported!</b>');
+    }
+}
+
+function clearCanvas() {
+    gGL.clear(gGL.COLOR_BUFFER_BIT);
+}
+
+function doGLDraw() {
+    initializeGL();
+    clearCanvas();
+}
